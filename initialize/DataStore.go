d93f64cbf970bedd99b27816996cc8943f32172e
black.dragon74 <nickk.2974@gmail.com>
2021-04-05 02:07:20 +0530

    Query data store API concurrently
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- initialize/DataStore.go
+++ initialize/DataStore.go
@@ -2,12 +2,14 @@ package initialize
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/black-dragon74/dms-api/config"
 	"github.com/black-dragon74/dms-api/types"
 	"github.com/black-dragon74/dms-api/utils"
 	"go.uber.org/zap"
 	"io/ioutil"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -15,15 +17,20 @@ func DataStore(lgr *zap.Logger, cfg *config.Config) (*types.DataStoreModel, erro
 	lgr.Info("[Initilaize] [DataStore] Loading stores in memory")
 	store := &types.DataStoreModel{}
 
-	// Load both the stores into memory
-	err := loadContactsStore(lgr, &store.ContactsData)
-	if err != nil {
-		return nil, err
-	}
+	// Use waitgroups to keep track of sub-goroutines
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
 
-	err = loadMessStore(lgr, &store.MessMenuData)
-	if err != nil {
-		return nil, err
+	// Load both the stores into memory, concurrently
+	go loadContactsStore(lgr, &store.ContactsData, wg)
+	go loadMessStore(lgr, &store.MessMenuData, wg)
+
+	// Wait for go-routines to exit
+	wg.Wait()
+
+	// Validate
+	if len(store.ContactsData) == 0 || store.MessMenuData.LastUpdatedAt == "" {
+		return nil, errors.New("failed to init the stores")
 	}
 
 	// Watch for changes if requested
@@ -45,22 +52,27 @@ func watchStoreForChanges(store *types.DataStoreModel, lgr *zap.Logger) {
 		select {
 		case <-messTicker.C:
 			lgr.Info("[Initialize] [DataStore] [WatchStoreForChanges] [MessTicker] Tick")
-			_ = loadMessStore(lgr, &store.MessMenuData)
+			loadMessStore(lgr, &store.MessMenuData, nil)
 
 		case <-contactsTicker.C:
 			lgr.Info("[Initialize] [DataStore] [WatchStoreForChanges] [ContactsTicker] Tick")
-			_ = loadContactsStore(lgr, &store.ContactsData)
+			loadContactsStore(lgr, &store.ContactsData, nil)
 		}
 	}
 }
 
 // loadContactsStore queries the contacts data store and loads the response into memory
-func loadContactsStore(lgr *zap.Logger, store *[]types.ContactsModel) error {
+func loadContactsStore(lgr *zap.Logger, store *[]types.ContactsModel, wg *sync.WaitGroup) {
+	// Notify the wait group, it will be nil if called by monitoring go-routine
+	if wg != nil {
+		defer wg.Done()
+	}
+
 	// Fetch the contacts store
 	resp, err := http.Get(utils.URLContactsDataStore)
 	if err != nil {
 		lgr.Error("[Initialize] [DataStore] Unable to fetch the contacts data store")
-		return err
+		return
 	}
 	defer resp.Body.Close()
 
@@ -68,25 +80,28 @@ func loadContactsStore(lgr *zap.Logger, store *[]types.ContactsModel) error {
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		lgr.Error("[Initialize] [DataStore] Unable to read the contacts store response")
-		return err
+		return
 	}
 
 	err = json.Unmarshal(data, store)
 	if err != nil {
 		lgr.Error("[Initialize] [DataStore] Contatcs store returned an unexpected response")
-		return err
+		return
 	}
 	lgr.Info("[Initialize] [DataStore] Contacts store loaded successfully")
-
-	return nil
 }
 
 // loadMessStore queries the mess menu data store and loads the response into memory
-func loadMessStore(lgr *zap.Logger, store *types.MessMenuModel) error {
+func loadMessStore(lgr *zap.Logger, store *types.MessMenuModel, wg *sync.WaitGroup) {
+	// Notify the wait group, it will be nil if called by monitoring go-routine
+	if wg != nil {
+		defer wg.Done()
+	}
+
 	resp, err := http.Get(utils.URLMessDataStore)
 	if err != nil {
 		lgr.Error("[Initialize] [DataStore] Unable to fetch the mess data store")
-		return err
+		return
 	}
 	defer resp.Body.Close()
 
@@ -94,15 +109,13 @@ func loadMessStore(lgr *zap.Logger, store *types.MessMenuModel) error {
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		lgr.Error("[Initialize] [DataStore] Unable to read the mess store response")
-		return err
+		return
 	}
 
 	err = json.Unmarshal(data, store)
 	if err != nil {
 		lgr.Error("[Initialize] [DataStore] Mess store returned an unexpected response")
-		return err
+		return
 	}
 	lgr.Info("[Initialize] [DataStore] Mess store loaded successfully")
-
-	return nil
 }
