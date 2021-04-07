37f76213a05e990a7e8025d660da325b52ca2bb9
black.dragon74 <nickk.2974@gmail.com>
2021-04-08 00:51:54 +0530

    Set proper status code on 404 requests
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- app/handler/FourOhFour.go
+++ app/handler/FourOhFour.go
@@ -12,6 +12,7 @@ func FourOhFourHandler(lgr *zap.Logger) http.HandlerFunc {
 
 		respData := `{"error":"404. Route not found"}`
 
+		writer.WriteHeader(404)
 		_, err := writer.Write([]byte(respData))
 		if err != nil {
 			lgr.Error(fmt.Sprintf("[Handler] [404] [Write] %s", err.Error()))
